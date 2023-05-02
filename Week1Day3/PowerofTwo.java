881be6b657d6ae1e5dedd8b70cb5aa128fe66afd
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-05-02 16:38:38 +0530

    Add files via upload

---
+++ Week1Day3/PowerofTwo.java
@@ -0,0 +1,25 @@
+package Week1Day3;
+
+import org.junit.Test;
+
+public class PowerofTwo {
+	
+	@Test
+	public void example1() {
+		int n=33;
+		powertwo(n);
+	}
+	
+private void powertwo(int n) {
+	while(n%2==0) {
+		n=n/2;
+	}
+	if(n==1) {
+		System.out.print("Is power of two");
+	}
+	else {
+		System.out.print("Not a power of two");
+	}
+		
+	}
+}
