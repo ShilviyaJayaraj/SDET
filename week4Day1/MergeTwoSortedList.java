ccc4d21c268068ddcc66be60fb9b6ebfedaa8aa3
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-05-22 22:50:21 +0530

    Update MergeTwoSortedList.java

--- week4Day1/MergeTwoSortedList.java
+++ week4Day1/MergeTwoSortedList.java
@@ -1,65 +1,93 @@
-package week4Day1;
+package  week4Day1;
 
-public class MergeTwoSortedList {
-       
-          public static void main (String[] args) {
-        	  LinkedList ll1=new LinkedList();
-        	  LinkedList ll2=new LinkedList();
-        	  ll1.new Node(1);
-        	  ll1.new Node(2);
-        	  ll1.new Node(4);
-        	  ll2.new Node(1);
-        	  ll2.new Node(3);
-        	  ll2.new Node(4);
-        	  System.out.println(ll1);
-        	  System.out.println(ll2);
-        	  mergeSort(ll1, ll2);
-        	  
-          }
+import week4Day1.linkedList;
+import week4Day1.linkedList.Node;
 
-		private static void mergeSort(LinkedList ll1, LinkedList ll2) {
-      	  LinkedList out=new LinkedList();
-      	    while(ll1!=null && ll2!=null) {
-      	    	if(ll1.val)
-      	    }
-			
-		}
+public class MergeTwoSortedList{
+    public static void main (String[] args) {
+    	linkedList ll1=new linkedList();
+    	linkedList ll2=new linkedList();
+    		ll1.add(1);
+    		ll1.add(2);
+    		ll1.add(3);
+    		ll2.add(1);
+    		ll2.add(3);
+    		ll2.add(4);
+    	System.out.println(ll1);
+    	System.out.println(ll2);
+    	ll1.start=new linkedList().mergeSort(ll1.start, ll2.start);
+    	ll1.printList();
+    }
 }
 
-class LinkedList{
-	class Node{
-		Node next=null;
-		int data;
-		
-		Node(int data){
-			this.data=data;
-			next=null;
-		}
-		Node() {
-			this.next = null;
-			
-		}
-		
-	}
-	Node start =null;
-	int size =0;
-	
-	/*
-	 * void add(String data) { Node node=new Node(); this.data=data; if(start==null)
-	 * { start=node; } else { Node current=start; while(current.next!=null) {
-	 * current=current.next; } current.next = node; } size++; }
-	 */
-	
-	@Override
-	  public String toString () {
-	    StringBuffer sb = new StringBuffer();
-	    sb.append("[");
-	    Node current = start;
-	    while (current != null) {
-	      sb.append(current.data + ", ");
-	      current = current.next;
-	    }
-	    sb.append("] --> " + size);
-	    return sb.toString();
-	  }
+class linkedList{
+class Node {
+    int data;
+    Node next=null;
+}
+ Node start=null;
+ 
+     void add(int data) {
+    	Node node=new Node();
+    	node.data = data;
+    	
+    	if(start==null) {
+    		start=node;
+    }
+    	else {
+    		Node current=start;
+    		while(current.next!=null) {
+    			current=current.next;
+    		}
+    		current.next=node;
+    	}
+}
+     void printList()
+     {
+         Node current = start;
+         while (current != null) {
+             System.out.print(current.data + " ");
+              current = current.next;
+         }
+         System.out.println();
+     }
+     
+     Node mergeSort(Node ll1, Node ll2) {
+    	 Node merge=new Node();
+    	 Node tail=merge;
+    	 while(true) {
+    	 if(ll1==null) {
+    		 tail.next=ll2;
+    		 break;
+    	 }
+    	 if(ll2==null) {
+    		 tail.next=ll1;
+    		 break;
+    	 }
+          if(ll1.data<=ll2.data) {
+        	  tail.next=ll1;
+              ll1=ll1.next;
+          }
+          else {
+        	  tail.next=ll2;
+        	  ll2=ll2.next;
+          }
+          tail=tail.next;
+         }
+       return merge.next;
+     }   
+     
+     @Override
+     public String toString() {
+     	StringBuffer sb=new StringBuffer();
+     	sb.append("[");
+     	Node current=start;
+     	while(current!=null) {
+     		sb.append(current.data+",");
+     		current = current.next;
+     	}
+     	sb.append("]");
+     	return sb.toString();
+     }
+
 }
