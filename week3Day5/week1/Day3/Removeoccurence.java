59c924559045d957e9547fa0db7cec30fd6910cf
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-06-14 21:37:04 +0530

    Add files via upload

---
+++ week3Day5/week1/Day3/Removeoccurence.java
@@ -0,0 +1,25 @@
+package week1.Day3;
+
+import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Removeoccurence {
+	@Test
+	public void example1() {
+		int[] num = {1,2,3,4,5,1};
+		int indextoremove = 5;
+		removevalue(num, indextoremove);
+	}
+
+	private void removevalue(int[] num, int indextoremove) {
+		int val = num[indextoremove];
+		List<Integer> lst = new ArrayList<Integer>();
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] != val) {
+				lst.add(num[i]);
+			}
+		}
+		System.out.print(lst);
+	}
+}
