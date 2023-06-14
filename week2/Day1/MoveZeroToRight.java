d3694a313c0728951ace02503496f5ebdfbf0d84
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-06-14 21:43:16 +0530

    Add files via upload

---
+++ week2/Day1/MoveZeroToRight.java
@@ -0,0 +1,32 @@
+package week2.Day1;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class MoveZeroToRight {
+  @Test
+  public void movezero() {
+	  int[] nums= {0,1,0,3,0,12,0,6,7,0,8};
+	  zeroToRight(nums);
+  }
+
+private void zeroToRight(int[] nums) {
+	int left=0, right=1;
+	int temp;
+	while(right<nums.length) {
+		if(nums[left]!=0 && nums[right]!=0) {
+			left++;
+			right++;
+		}
+		else if(nums[left]==0 && nums[right]==0)
+			right++;
+		else {
+			temp=nums[left];
+			nums[left++]=nums[right];
+			nums[right++]=temp;
+		}
+	}
+	System.out.print(Arrays.toString(nums));
+}
+}
---
+++ week2/Day4/Reverseprefix.java
@@ -0,0 +1,49 @@
+package week2.Day4;
+
+import org.junit.Test;
+
+/*
+ * loop and find index of given character
+ * create temp variable and add last index to first using 2 pointer
+ * then print it
+ */
+
+public class Reverseprefix {
+	 @Test
+	 public void example1() {
+		 String word="abcdefd";
+		 char ch='d';
+		 reversePrefixOfWord(word, ch);
+	 }
+
+	private void reversePrefixOfWord(String word, char ch) {
+		//System.out.print(word.length());
+		int count =0, index=0;
+		//String out=new String();
+		for(int i=0; i<word.length();i++) {
+			if(count == 0 && ch==word.charAt(i)) {
+				count=count+1;
+			    index=i;
+				System.out.println(index);
+			}
+		}
+		
+		StringBuilder sb=new StringBuilder(word.substring(0, index+1));
+        sb.reverse();
+        if(index+1<word.length()) {
+        	sb.append(word.substring(index+1));
+        	
+        }
+        System.out.print(sb);
+	}
+
+}
+
+
+
+// count=0; 
+/*
+ * while(count<index+1) { // int left=0; int right=index; if(index>0) { String
+ * temp=""; temp=temp+word.charAt(right); right--; // System.out.print(right);
+ * System.out.print(temp); } count++; }
+ */
