b5e739cdc723a4d4f76310476011b09d7e9875a3
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-06-14 21:41:14 +0530

    Add files via upload

---
+++ week1/Day5/DiamondpatternA2F.java
@@ -0,0 +1,37 @@
+package week1.Day5;
+
+public class DiamondpatternA2F {
+  
+	public static void main(String[] args) {
+		int letter=65;
+		for (int i=0; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				if(k==0||k==i) {
+					System.out.print((char)(letter+i)+" ");
+				}
+				else {
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		for (int i=4; i>=0; i--) {
+			for(int j=4-i; j>=0; j--) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				if(k==0||k==i) {
+					System.out.print((char)(letter+i)+" ");
+				}
+				else {
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
