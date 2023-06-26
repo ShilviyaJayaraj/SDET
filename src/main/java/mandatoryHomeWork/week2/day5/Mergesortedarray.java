920e2295b62a15e3fd419028985cf391628166f5
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-06-26 23:32:21 +0530

    Update Mergesortedarray.java

--- src/main/java/mandatoryHomeWork/week2/day5/Mergesortedarray.java
+++ src/main/java/mandatoryHomeWork/week2/day5/Mergesortedarray.java
@@ -17,29 +17,53 @@ import org.junit.Test;
 public class Mergesortedarray {
    @Test
    public void example1() {
-	   int[] num1= {1,2,3};
+	   int[] num1= {1,2,3,0,0,0};
 	   int[] num2= {2,5,6}; // { 1,2,2,3,5,6}
 	   mergeSortedArr(num1, num2);
    }
 
+  // @Test
+   public void example2() {
+	   int[] num1= {1};
+	   int[] num2= {}; // { 1,2,2,3,5,6}
+	   
+	   mergeSortedArr(num1, num2);
+   }
+   
 private void mergeSortedArr(int[] num1, int[] num2) {
-	       int[] out=new int[num1.length+num2.length];
-	       int pt1=0, pt2=0, i=0;
-	     //  System.out.print(out.length);
-	       while(pt1<num1.length || pt2<num2.length){
-	    	// if(pt1==num1.length)  
-	    	   if(num1[pt1]<num2[pt2]) {
-	    		 //  if()
-	    		   out[i++]=num1[pt1++];
-	    	   }
-	    	 /*  else if(num1[pt1]==num2[pt2]) {
-	    		*   out[i++]=num1[pt1++];
-	    		 *  out[i++]=num1[pt2++];
-	    	   }*/
-	    	   else {
-	    		   out[i++]=num1[pt2++];
-	    	   }
-	       }
-	       System.out.print(Arrays.toString(out));
+	    int pointer1=0;
+	    int pointer2=0;
+	    int m=1;
+		int n=0;
+	    	//System.out.println(Arrays.toString(num1));
+	    while(pointer1 < num1.length) {
+	    	if(num1[pointer1]>num2[pointer2]) {
+	    		int temp=num1[pointer1];
+	    		num1[pointer1++]=num2[pointer2];
+	    		num2[pointer2]=temp;
+	    	//	System.out.println("first if ");
+	    		//System.out.println(Arrays.toString(num1));
+		    	//System.out.println(Arrays.toString(num2));
+	    	}
+	    	
+	    		if(num1[pointer1]<=num2[pointer2] && num1[pointer1]>0) {
+		    		pointer1++;
+		    		//System.out.println("seconf if ");
+		    		//System.out.println(Arrays.toString(num1));
+			    	//System.out.println(Arrays.toString(num2));
+	    	}
+	    	if(num1[pointer1]==0) {
+	    		int temp=num1[pointer1];
+	    		num1[pointer1++]=num2[pointer2];
+	    		num2[pointer2++]=temp;
+	    		//System.out.println("third if ");
+	    		//System.out.println(Arrays.toString(num1));
+		    	//System.out.println(Arrays.toString(num2));
+	    	}
+	    	//System.out.println(Arrays.toString(num1));
+	    	//System.out.println(Arrays.toString(num2));
+	    }
+
+	    System.out.println(Arrays.toString(num1));
 }
 }
