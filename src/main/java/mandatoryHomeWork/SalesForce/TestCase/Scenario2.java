348e6b01c6dcf95a4be40b65652a42305271111c
ShilviyaJayaraj <131800135+ShilviyaJayaraj@users.noreply.github.com>
2023-08-20 13:23:52 +0530

    Update Scenario2.java

--- src/main/java/mandatoryHomeWork/SalesForce/TestCase/Scenario2.java
+++ src/main/java/mandatoryHomeWork/SalesForce/TestCase/Scenario2.java
@@ -3,6 +3,20 @@ package TestCase;
 public class Scenario2 {
 
 	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver(options);
+		driver.manage().window().maximize();
 		
+		driver.get("https://google-bf-dev-ed.develop.my.salesforce.com/");
+
+		driver.findElement(By.id("username")).sendKeys("muthusubramanian91@sdet.testleaf.com");
+		driver.findElement(By.id("password")).sendKeys("TestLeaf@1012");
+		driver.findElement(By.id("Login")).click();
+        driver.findElement(By.className("slds-r5")).click();
+        driver.findElement(By.xpath("//button[text()='View All']")).click();
+        driver.findElement(By.xpath("//input[@part='input']")).sendKeys("Service");
+        driver.findElement(By.xpath("//mark[text()='Service']")).click();
 	}
 }
